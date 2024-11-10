f7ac91f09a4504e9e2dae3a88c34c0e68c9ea99a
mike o'brien <mpobrien005@gmail.com>
2024-11-10 14:54:54 -0500

    Bunch of fixups (#2)
    * make wiredtiger a submodule
    
    * clean up build.rs
    
    * fix up directory structure
    
    * switch to new workspace layout
    
    * add tests and cleanup
    
    * remove debug lines

---
+++ libwiredtiger/build.rs
@@ -0,0 +1,75 @@
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn build_wt() -> std::io::Result<()> {
+    let wt_dir = "wiredtiger";
+    let build_dir = format!("{wt_dir}/build");
+    Command::new("cmake")
+        .arg("-DENABLE_STATIC=1")
+        .arg("-S")
+        .arg(wt_dir)
+        .arg("-B")
+        .arg(&build_dir)
+        .output()?;
+    Command::new("cmake")
+        .arg("--build")
+        .arg(&build_dir)
+        .arg("-j16")
+        .output()?;
+    Ok(())
+}
+
+fn bindgen_wt() {
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate bindings for.
+        .header(format!("wiredtiger/build/include/wiredtiger.h"))
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Failed to write bindings");
+}
+
+fn main() {
+    if !Path::new("wiredtiger/LICENSE").exists() {
+        update_submodules();
+    }
+    build_wt().expect("Failed to build wiredtiger");
+
+    bindgen_wt();
+
+    // Tell cargo to look for shared libraries in the specified directory.
+    // Note that this search path is relative to the crate root, not the workspace root.
+    println!("cargo:rustc-link-search=libwiredtiger/wiredtiger/build");
+
+    // Tell cargo to tell rustc to statically link with the wiredtiger library.
+    // This requires that WT was configured with the -DENABLE_STATIC=1 option to cmake.
+    println!("cargo:rustc-link-lib=static=wiredtiger");
+}
+
+fn update_submodules() {
+    let program = "git";
+    let dir = "../";
+    let args = ["submodule", "update", "--init"];
+    println!(
+        "Running command: \"{} {}\" in dir: {}",
+        program,
+        args.join(" "),
+        dir
+    );
+    let ret = Command::new(program).current_dir(dir).args(args).status();
+
+    match ret.map(|status| (status.success(), status.code())) {
+        Ok((true, _)) => (),
+        Ok((false, Some(c))) => panic!("Command failed with error code {}", c),
+        Ok((false, None)) => panic!("Command got killed"),
+        Err(e) => panic!("Command failed with error: {}", e),
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,38 @@
-mod wiredtiger;
-
-use wiredtiger::{
-    WT_CONNECTION,
-    WT_SESSION,
-    WT_CURSOR,
-    WT_EVENT_HANDLER,
-};
-use wiredtiger::{
-    wiredtiger_open,
-    wiredtiger_strerror,
-};
-use wiredtiger::WT_NOTFOUND;
-
-use std::ffi::{ CString, CStr };
-use std::ptr;
-use std::io;
+use libc::{self, c_char, c_void, size_t};
+use libwiredtiger as wtffi;
+use std::ffi::{CStr, CString};
 use std::os::raw;
+use std::ptr;
 
-fn get_error(result: i32) -> io::Error {
-    let err_msg = unsafe { CStr::from_ptr(wiredtiger_strerror(result)) };
-    return io::Error::other(err_msg.to_str().unwrap().to_owned());
+macro_rules! unwrap_or_panic {
+    ($option:expr, $( $args:expr ),* ) => {
+        match $option {
+            Some(f) => f($($args),*),
+            None => panic!("function pointer is None"),
+        }
+    };
 }
 
-fn make_result<T>(result: i32, value: T) -> Result<T, io::Error> {
+pub(crate) unsafe fn from_cstr(ptr: *const c_char) -> String {
+    let cstr = CStr::from_ptr(ptr as *const _);
+    String::from_utf8_lossy(cstr.to_bytes()).into_owned()
+}
+
+pub fn error_message(result: i32) -> String {
+    unsafe {
+        let msg = wtffi::wiredtiger_strerror(result);
+        from_cstr(msg)
+    }
+}
+
+fn get_error(result: i32) -> Error {
+    Error {
+        message: error_message(result),
+    }
+}
+
+// TODO make this a macro?
+fn make_result<T>(result: i32, value: T) -> Result<T, Error> {
     if result != 0 {
         return Err(get_error(result));
     }
@@ -30,178 +40,195 @@ fn make_result<T>(result: i32, value: T) -> Result<T, io::Error> {
 }
 
 pub struct Connection {
-    conn: *mut WT_CONNECTION,
+    conn: *mut wtffi::WT_CONNECTION,
 }
 
 pub struct Session {
-    session: *mut WT_SESSION,
+    session: *mut wtffi::WT_SESSION,
 }
 
 pub struct Cursor {
-    cursor: *mut WT_CURSOR,
+    cursor: *mut wtffi::WT_CURSOR,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Error {
+    message: String,
+}
+
+impl Error {
+    fn new(message: String) -> Self {
+        Self { message }
+    }
 }
 
 impl Connection {
-    pub fn open(filename: &str, options: &str) -> Result<Self, io::Error> {
-        let mut conn: *mut WT_CONNECTION = ptr::null_mut();
+    pub fn open(filename: &str, options: &str) -> Result<Self, Error> {
+        // outparam destination for wiredtiger_open()
+        let mut conn: *mut wtffi::WT_CONNECTION = ptr::null_mut();
+
         let options = CString::new(options).unwrap();
         let dbpath = CString::new(filename).unwrap();
-        let event_handler: *const WT_EVENT_HANDLER = ptr::null();
-        let result: i32;
-        unsafe {
-            result = wiredtiger_open(
+
+        // TODO: support a non-null event handler.
+        let event_handler: *const wtffi::WT_EVENT_HANDLER = ptr::null();
+
+        let result = unsafe {
+            wtffi::wiredtiger_open(
                 dbpath.as_ptr(),
-                event_handler as *mut WT_EVENT_HANDLER,
+                event_handler as *mut wtffi::WT_EVENT_HANDLER,
                 options.as_ptr(),
                 &mut conn,
-            );
+            )
         };
         make_result(result, Connection { conn })
     }
 
-    pub fn create_session(&self) -> Result<Session, io::Error> {
-        let mut session: *mut WT_SESSION = ptr::null_mut();
-        let event_handler: *mut WT_EVENT_HANDLER = ptr::null_mut();
-        let result: i32;
+    pub fn open_session(&self) -> Result<Session, Error> {
+        let mut session: *mut wtffi::WT_SESSION = ptr::null_mut();
+        let event_handler: *mut wtffi::WT_EVENT_HANDLER = ptr::null_mut();
         unsafe {
-            result = (self.conn.as_ref().unwrap().open_session.unwrap())(
+            let result = unwrap_or_panic!(
+                (*self.conn).open_session,
                 self.conn,
                 event_handler,
                 ptr::null(),
-                &mut session,
+                &mut session
             );
+            return make_result(result, Session { session });
         }
-        make_result(result, Session { session })
     }
 }
 
 impl Session {
-    pub fn create(&self, name: &str, config: &str) -> Result<(), io::Error> {
+    pub fn create(&self, name: &str, config: &str) -> Result<(), Error> {
         let name = CString::new(name).unwrap();
         let config = CString::new(config).unwrap();
-        let result: i32;
-        unsafe {
-            result = (self.session.as_ref().unwrap().create.unwrap())(
-                self.session as *mut WT_SESSION,
+        let result = unsafe {
+            unwrap_or_panic!(
+                (*self.session).create,
+                self.session as *mut wtffi::WT_SESSION,
                 name.as_ptr(),
-                config.as_ptr(),
-            );
-        }
+                config.as_ptr()
+            )
+        };
         make_result(result, ())
     }
 
-    pub fn cursor(&self, uri: &str) -> Result<Cursor, io::Error> {
+    pub fn open_cursor(&self, uri: &str) -> Result<Cursor, Error> {
         let uri = CString::new(uri).unwrap();
-        let mut cursor: *mut WT_CURSOR = ptr::null_mut();
-        let cursor_null: *const WT_CURSOR = ptr::null();
-        let result: i32;
-        unsafe {
-            result = (self.session.as_ref().unwrap().open_cursor.unwrap())(
+        let mut cursor: *mut wtffi::WT_CURSOR = ptr::null_mut();
+        let cursor_null: *const wtffi::WT_CURSOR = ptr::null();
+        let result = unsafe {
+            unwrap_or_panic!(
+                (*self.session).open_cursor,
                 self.session,
                 uri.as_ptr(),
-                cursor_null as *mut WT_CURSOR,
+                cursor_null as *mut wtffi::WT_CURSOR,
                 ptr::null(),
-                &mut cursor,
-            );
-        }
-        make_result(result, Cursor{cursor})
+                &mut cursor
+            )
+        };
+        make_result(result, Cursor { cursor })
     }
 }
 
 impl Cursor {
     pub fn scan(&self) {
-        let key: *mut WT_SESSION = ptr::null_mut();
-        let val: *mut WT_SESSION = ptr::null_mut();
+        let key: *mut wtffi::WT_SESSION = ptr::null_mut();
+        let val: *mut wtffi::WT_SESSION = ptr::null_mut();
         unsafe {
-            self.cursor.as_ref().unwrap().reset.unwrap()(
-                self.cursor as *mut WT_CURSOR
-            );
+            unwrap_or_panic!((*self.cursor).reset, self.cursor);
             loop {
-                let result = self.cursor.as_ref().unwrap().next.unwrap()(
-                    self.cursor as *mut WT_CURSOR
-                );
-                if result != 0 { break };
-                self.cursor.as_ref().unwrap().get_key.unwrap()(
-                    self.cursor as *mut WT_CURSOR,
-                    &key
-                );
-                self.cursor.as_ref().unwrap().get_key.unwrap()(
-                    self.cursor as *mut WT_CURSOR,
-                    &val
-                );
+                let result = unwrap_or_panic!((*self.cursor).next, self.cursor);
+                if result != 0 {
+                    break;
+                };
+                unwrap_or_panic!((*self.cursor).get_key, self.cursor, &key);
+                unwrap_or_panic!((*self.cursor).get_key, self.cursor, &val);
             }
         }
     }
 
-    pub fn set(&self, key: &str, value: &str) -> Result<(), io::Error> {
-        let result: i32;
+    pub fn set(&self, key: &str, value: &str) -> Result<(), Error> {
         let ckey = CString::new(key).unwrap();
         let cval = CString::new(value).unwrap();
-        unsafe {
-            self.cursor.as_ref().unwrap().set_key.unwrap()(
-                self.cursor as *mut WT_CURSOR,
-                ckey.as_ptr(),
-            );
-            self.cursor.as_ref().unwrap().set_value.unwrap()(
-                self.cursor as *mut WT_CURSOR,
-                cval.as_ptr(),
-            );
-            result = self.cursor.as_ref().unwrap().insert.unwrap()(
-                self.cursor as *mut WT_CURSOR
-            );
-        }
+
+        let result = unsafe {
+            unwrap_or_panic!((*self.cursor).set_key, self.cursor, ckey.as_ptr());
+            unwrap_or_panic!((*self.cursor).set_value, self.cursor, cval.as_ptr());
+            unwrap_or_panic!((*self.cursor).insert, self.cursor)
+        };
         make_result(result, ())
     }
 
-    pub fn search(&self, key: &str) -> Result<Option<String>, io::Error> {
-        let mut result: i32;
+    pub fn search(&self, key: &str) -> Result<Option<String>, Error> {
         let ckey = CString::new(key).unwrap();
         let mut val: *mut raw::c_char = ptr::null_mut();
+        let result = unsafe {
+            unwrap_or_panic!((*self.cursor).set_key, self.cursor, ckey.as_ptr());
+            unwrap_or_panic!((*self.cursor).search, self.cursor)
+        };
+        if result == wtffi::WT_NOTFOUND {
+            return Ok(None);
+        }
+        if result != 0 {
+            return Err(get_error(result));
+        }
         unsafe {
-            self.cursor.as_ref().unwrap().set_key.unwrap()(
-                self.cursor as *mut WT_CURSOR,
-                ckey.as_ptr(),
-            );
-            result = self.cursor.as_ref().unwrap().search.unwrap()(
-                self.cursor as *mut WT_CURSOR
-            );
-            if result == WT_NOTFOUND {
-                return Ok(None);
-            }
-            if result !=0 {
+            let result = unwrap_or_panic!((*self.cursor).get_value, self.cursor, &mut val);
+            if result != 0 {
                 return Err(get_error(result));
             }
-            result = self.cursor.as_ref().unwrap().get_value.unwrap()(
-                self.cursor as *mut WT_CURSOR,
-                &mut val,
-            );
             let owned_val = CStr::from_ptr(val).to_string_lossy().into_owned();
-            make_result(result, Some(owned_val))
+            return make_result(result, Some(owned_val));
         }
     }
 }
 
 impl Drop for Connection {
     fn drop(&mut self) {
-        println!("closing connection!");
         unsafe {
-            // *WT_CONNECTION.as_ref() -> Option<&WT_CONNECTION>
-            // conn->close(conn, null);
-            (self.conn.as_ref().unwrap().close.unwrap())(self.conn, std::ptr::null());
+            unwrap_or_panic!((*self.conn).close, self.conn, std::ptr::null());
         }
     }
 }
 
 impl Drop for Session {
     fn drop(&mut self) {
-        //todo!()
+        unsafe {
+            unwrap_or_panic!((*self.session).close, self.session, std::ptr::null());
+        }
     }
 }
 
 impl Drop for Cursor {
     fn drop(&mut self) {
-        // WT_CONNECTION::close
-        // (self.conn as WT_CONNECTION)
-        //todo!()
+        unsafe {
+            unwrap_or_panic!((*self.cursor).close, self.cursor);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use assert_ok::assert_ok;
+    #[test]
+    fn test() {
+        // Create a temp dir to put the WT files into, open a connection to it.
+        let temp_dir = tempfile::tempdir().unwrap();
+        let conn = Connection::open(temp_dir.path().to_str().unwrap(), "create").unwrap();
+        let session = conn.open_session().unwrap();
+
+        // Create a new table string keys and string values
+        let create_result = session.create("table:mytable", "key_format=S,value_format=S");
+        assert_ok!(create_result);
+
+        let cursor = assert_ok!(session.open_cursor("table:mytable"));
+        assert_ok!(cursor.set("tyler", "brock"));
+        assert_ok!(cursor.set("mike", "obrien"));
+        println!("tyler: {:?}", cursor.search("tyler").unwrap());
+        println!("mike: {:?}", cursor.search("mike").unwrap());
     }
 }
